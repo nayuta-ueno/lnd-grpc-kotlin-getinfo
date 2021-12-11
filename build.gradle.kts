10e0949fac57e29b9e1efd008ac7e02a2fc987e7
ueno <ueno@nayuta.co>
2021-12-11 18:41:54 +0900

    lnrpc getinfo

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.google.protobuf.gradle.*
 
 plugins {
+    id("com.google.protobuf") version "0.8.18"
     kotlin("jvm") version "1.5.10"
     application
 }
@@ -14,6 +16,13 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("io.grpc:grpc-kotlin-stub:1.2.0")
+    implementation("io.grpc:grpc-netty:1.42.1")
+    implementation("io.grpc:grpc-protobuf:1.42.1")
+    implementation("io.netty:netty-handler:4.1.71.Final")
+    implementation("com.google.protobuf:protobuf-kotlin:3.19.1")
+    implementation("commons-codec:commons-codec:1.15")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0-RC2")
 }
 
 tasks.test {
@@ -26,4 +35,29 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.39.0"
+        }
+        id("grpckt") {
+            artifact = "io.grpc:protoc-gen-grpc-kotlin:1.2.0:jdk7@jar"
+        }
+    }
+    generateProtoTasks {
+        all().forEach {
+            it.plugins {
+                id("grpc")
+                id("grpckt")
+            }
+            it.builtins {
+                id("kotlin")
+            }
+        }
+    }
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,50 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import io.grpc.CallCredentials
+import io.grpc.ManagedChannel
+import io.grpc.Metadata
+import io.grpc.Status
+import io.grpc.netty.GrpcSslContexts
+import io.grpc.netty.NettyChannelBuilder
+import io.netty.handler.ssl.SslContext
+import lnrpc.LightningGrpc
+import lnrpc.LightningGrpc.LightningBlockingStub
+import org.apache.commons.codec.binary.Hex
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.concurrent.Executor
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+internal class MacaroonCallCredential(private val macaroon: String) : CallCredentials() {
+    override fun applyRequestMetadata(requestInfo: RequestInfo, executor: Executor, metadataApplier: MetadataApplier) {
+        executor.execute {
+            try {
+                val headers = Metadata()
+                val macaroonKey: Metadata.Key<String> =
+                    Metadata.Key.of("macaroon", Metadata.ASCII_STRING_MARSHALLER)
+                headers.put(macaroonKey, macaroon)
+                metadataApplier.apply(headers)
+            } catch (e: Throwable) {
+                metadataApplier.fail(Status.UNAUTHENTICATED.withCause(e))
+            }
+        }
+    }
+
+    override fun thisUsesUnstableApi() {}
+}
+
+private const val CERT_PATH = "/home/ueno/Lightning/s1/lnddata/tls.cert"
+private const val MACAROON_PATH = "/home/ueno/Lightning/s1/lnddata/data/chain/bitcoin/signet/admin.macaroon"
+private const val HOST = "localhost"
+private const val PORT = 10009
+
+fun main() {
+    val sslContext = GrpcSslContexts.forClient().trustManager(File(CERT_PATH)).build()
+    val channelBuilder = NettyChannelBuilder.forAddress(HOST, PORT)
+    val channel = channelBuilder.sslContext(sslContext).build()
+    val macaroon = Hex.encodeHexString(Files.readAllBytes(Paths.get(MACAROON_PATH)))
+    val stub = LightningGrpc
+        .newBlockingStub(channel)
+        .withCallCredentials(MacaroonCallCredential(macaroon))
+
+    val response: lnrpc.LightningOuterClass.GetInfoResponse = stub.getInfo(lnrpc.LightningOuterClass.GetInfoRequest.getDefaultInstance())
+    println("node_id: ${response.getIdentityPubkey()}")
 }
